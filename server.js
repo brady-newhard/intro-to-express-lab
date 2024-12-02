f94255bd7a08216e737286dc9a7d7a612b9df7c7
Brady Newhard <brady.russell@gmail.com>
2024-12-01 22:57:15 -0500

    Completed Ex 4

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 const express = require('express');
+const morgan = require('morgan');
+
 const app = express();
 
-// Exercise 1 //
+app.use(morgan('dev'));
 
-app.get('/greetings/:name', (req, res) => {
-    res.send(`Wazzzzzup!, ${req.params.name}!`);
+// ------------Exercise 1------------ //
+
+app.get('/greetings/:username', (req, res) => {
+    res.send(`Wazzzzzup!, ${req.params.username}!`)
+    
 });
 
-// Exercise 2 //
+// Avisa's logic //
+app.get('/greetings/:username', (req, res) => {
+    const name = req.params.username
+    res.send(`Yo, yo, yo!, ${name}!`)
+   
+});
+
+// ------------Exercise 2------------ //
 
 app.get('/roll/:number', (req, res) => {
     const number = req.params.number
@@ -20,7 +32,7 @@ app.get('/roll/:number', (req, res) => {
     }
 });
 
-// Exercise 3 //
+// ------------Exercise 3------------ //
 
 const collectibles = [
     { name: 'shiny ball', price: 5.95 },
@@ -30,19 +42,46 @@ const collectibles = [
   
   app.get('/collectibles/:index', (req, res) => {
     const index = parseInt(req.params.index);
-
     if (index < 0 || index >= collectibles.length) {
         res.send('This item is not yet in stock. Check back soon!');
     } else {
-        const collectible = collectibles[index];
-        res.send(`So, you want the ${collectible.name}? For ${collectible.price}, it can be yours!`);
+        const item = collectibles[index];
+        res.send(`So, you want the ${item.name}? For ${item.price}, it can be yours!`);
     }
 });
 
-// Exercise 4 //
+// ------------Exercise 4------------ //
 
+const shoes = [
+    { name: "Birkenstocks", price: 50, type: "sandal" },
+    { name: "Air Jordans", price: 500, type: "sneaker" },
+    { name: "Air Mahomeses", price: 501, type: "sneaker" },
+    { name: "Utility Boots", price: 20, type: "boot" },
+    { name: "Velcro Sandals", price: 15, type: "sandal" },
+    { name: "Jet Boots", price: 1000, type: "boot" },
+    { name: "Fifty-Inch Heels", price: 175, type: "heel" }
+];
 
+app.get('/shoes', (req, res) => {
+    let filterShoes = shoes
+    const minPrice = req.query.minPrice;
+    const maxPrice = req.query.maxPrice;
+    const type = req.query.type;
+
+    if (minPrice) {
+        filterShoes = filterShoes.filter(shoe => shoe.price >= minPrice);
+    }
+    if (maxPrice) {
+        filterShoes = filterShoes.filter(shoe => shoe.price <= maxPrice);
+    }
+    if (type) {
+        filterShoes = filterShoes.filter(shoe => shoe.type === type);
+    }
+
+    res.send(`Results: ${filterShoes.map(shoe => shoe.name).join(', ')}`);  
+})
 
 app.listen(3000, () => {
-    console.log('Listening on port 3000')
+    console.log('🎧Listening on http://localhost:3000')
 })
+    
